f0eb5107cb4759565916ed3239037a5547f0b870
Jersen Meim <51324287+Jersenmeim@users.noreply.github.com>
2021-06-11 17:59:33 +0800

    update controller and yaml

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,13 @@
-const { Router } = require('express');
+const {
+    Router
+} = require('express');
 
 const controller = require('../controllers/movies');
 
 const router = Router();
 
 router.get('/', controller.getMovies);
+router.get('/add-movie', controller.getAddMovies);
+router.post('/add-movie', controller.postMovies);
 
 module.exports = router;
